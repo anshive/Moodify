aca6df344155d42619e156741528ee90b7661b0e
Anshive Vasist <anshivevasist@gmail.com>
2024-08-10 23:43:35 +0530

    Added Available genres from Spotify

--- moody-music/src/components/MoodInput.js
+++ moody-music/src/components/MoodInput.js
@@ -3,7 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 import { fetchSongs } from './SongSuggestions';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';// Adjust this Url according to your backend Djnago API host URL
+const API_BASE_URL = 'http://127.0.0.1:8000/api';
 
 function MoodInput({ onMoodSelect, onSongsFetched }) {
   const [inputMethod, setInputMethod] = useState('dropdown');
@@ -77,20 +77,39 @@ function MoodInput({ onMoodSelect, onSongsFetched }) {
 
       {inputMethod === 'dropdown' && (
         <Form.Group controlId="moodSelect">
-          <Form.Label>Select Your Mood</Form.Label>
-          <Form.Control
-            as="select"
-            value={selectedMood}
-            onChange={(e) => setSelectedMood(e.target.value)}
-          >
-            {/* basic moods fetching like happy, sad etc */}
-            <option value="">Choose...</option>
-            <option value="happy">Happy</option>
-            <option value="sad">Sad</option>
-            <option value="energetic">Energetic</option>
-            <option value="calm">Calm</option>
-          </Form.Control>
-        </Form.Group>
+        <Form.Label>Select Your Mood/Genre</Form.Label>
+        <Form.Control
+          as="select"
+          value={selectedMood}
+          onChange={(e) => setSelectedMood(e.target.value)}
+        >
+          <option value="">Choose...</option>
+          {[
+            "acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", 
+            "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", 
+            "british", "cantopop", "chicago-house", "children", "chill", "classical", 
+            "club", "comedy", "country", "dance", "dancehall", "death-metal", 
+            "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", 
+            "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", 
+            "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", 
+            "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", 
+            "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", 
+            "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", 
+            "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", 
+            "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", 
+            "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", 
+            "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", 
+            "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", 
+            "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", 
+            "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", 
+            "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", 
+            "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", 
+            "trance", "trip-hop", "turkish", "work-out", "world-music"
+          ].map((mood) => (
+            <option key={mood} value={mood}>{mood}</option>
+          ))}
+        </Form.Control>
+      </Form.Group>
       )}
 
       {inputMethod === 'text' && (
